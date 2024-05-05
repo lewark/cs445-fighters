89d230bd0e956e79896f59a929bbe3852fed7fd6
Everett Lewark <78086132+lewark@users.noreply.github.com>
2024-05-05 12:28:25 -0600

    Perform small a2c grid search

--- fighters/a2c/a2c.py
+++ fighters/a2c/a2c.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         "policy": ['CnnPolicy'],
 #        "policy": ['CnnPolicy', 'MlpPolicy'],
         "learning_rate": [0.1, 0.01, 0.001, 0.0001],
-#        "n_steps": [1024], #, 32, 64, 128, 256]
+        "n_steps": [5, 16, 32, 64]
 #        "use_rms_prop": [True, False],
 #        "gamma": [0.99, 0.5],
 #        "gae_lambda": [1, 0.5, 0],
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     for model_options in model_setups:
         print(model_options)
         learning_rate = model_options["learning_rate"]
-        label = f"A2C_{learning_rate}"
+        n_steps = model_options["n_steps"]
+        label = f"A2C_{learning_rate}_{n_steps}"
 
-        train_model(A2C, env, model_options, total_timesteps=500000, log_interval=1, n_eval_episodes=25, tb_log_name=label)
+        train_model(A2C, env, model_options, total_timesteps=500000, log_interval=100, n_eval_episodes=25, tb_log_name=label, device="auto")
     env.close()
