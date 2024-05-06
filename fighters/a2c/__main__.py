f3630f3ab65cf9075abb81416adfab3e82eabe35
Everett Lewark <78086132+lewark@users.noreply.github.com>
2024-05-05 19:29:51 -0600

    Rename main file, implement run_checkpoint

--- fighters/a2c/__main__.py
+++ fighters/a2c/__main__.py
@@ -1,15 +1,14 @@
 from ..common.fighter_envs import StreetFighter, make_env
 from ..common.train import train_model, get_hyperparam_combos
+from stable_baselines3 import A2C, PPO
 
 
 if __name__ == "__main__":
-    from stable_baselines3 import A2C, PPO
-
     params = {
         "policy": ['CnnPolicy'],
 #        "policy": ['CnnPolicy', 'MlpPolicy'],
-        "learning_rate": [0.1, 0.01, 0.001, 0.0001],
-        "n_steps": [5, 16, 32, 64]
+        "learning_rate": [0.01, 0.001, 0.0001, 0.00001],
+        "n_steps": [5], #, 16]
 #        "use_rms_prop": [True, False],
 #        "gamma": [0.99, 0.5],
 #        "gae_lambda": [1, 0.5, 0],
@@ -29,5 +28,5 @@ if __name__ == "__main__":
         n_steps = model_options["n_steps"]
         label = f"A2C_{learning_rate}_{n_steps}"
 
-        train_model(A2C, env, model_options, total_timesteps=500000, log_interval=100, n_eval_episodes=25, tb_log_name=label, device="auto")
+        train_model(A2C, env, model_options, total_timesteps=1000000, log_interval=100, n_eval_episodes=25, tb_log_name=label, device="auto")
     env.close()
--- fighters/a2c/run_checkpoint.py
+++ fighters/a2c/run_checkpoint.py
@@ -1,5 +1,14 @@
 import sys
 
 from stable_baselines3 import A2C
+from ..common.fighter_envs import StreetFighter, make_env
 
 model = A2C.load(sys.argv[1])
+
+# https://stable-baselines3.readthedocs.io/en/master/guide/examples.html
+vec_env = make_env(StreetFighter, n_procs=0, render_mode="human")
+obs = vec_env.reset()
+while True:
+    action, _states = model.predict(obs, deterministic=False)
+    obs, rewards, dones, info = vec_env.step(action)
+    #vec_env.render("human")
