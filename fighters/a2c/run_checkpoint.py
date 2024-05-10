1ef89b7170ffdd890730f8792773c6918aa9cae4
Everett Lewark <78086132+lewark@users.noreply.github.com>
2024-05-09 18:26:43 -0600

    Add optional video output argument

--- fighters/a2c/run_checkpoint.py
+++ fighters/a2c/run_checkpoint.py
@@ -4,21 +4,31 @@ from stable_baselines3 import A2C
 from ..common.fighter_envs import StreetFighter, make_env
 from gymnasium.wrappers import RecordVideo
 
-VIDEO_DIR = "results/video"
+model_filename = sys.argv[1]
+print("Loading model from", model_filename)
 
-model = A2C.load(sys.argv[1])
+model = A2C.load(model_filename)
+
+video_dir = None
+if len(sys.argv) >= 3:
+    video_dir = sys.argv[2]
+    print("Writing video files to", video_dir)
 
 # https://stable-baselines3.readthedocs.io/en/master/guide/examples.html
 
-def make_rec_env():
-    return StreetFighter(render_mode="rgb_array", video_folder=VIDEO_DIR)
+def make_fighter_env():
+    if video_dir is None:
+        return StreetFighter(render_mode="human")
+    return StreetFighter(render_mode="rgb_array", video_folder=video_dir)
 
 try:
-    vec_env =  make_env(make_rec_env, n_procs=0)
+    vec_env = make_env(make_fighter_env, n_procs=0)
     obs = vec_env.reset()
     while True:
         action, _states = model.predict(obs, deterministic=False)
         obs, rewards, dones, info = vec_env.step(action)
+        #if video_dir is not None:
+        #    vec_env.render()
         if dones[0]:
             break
 finally:
